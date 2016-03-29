c2dbf539954ac3cdb2cf55dac485246a4807ae4b
frankjoshua <frankjoshua@gmail.com>
2016-03-29 00:12:45 -0500

    Testing RC

--- pocketbot_rc_mecanum/pocketbot_rc_mecanum.ino
+++ pocketbot_rc_mecanum/pocketbot_rc_mecanum.ino
@@ -2,12 +2,14 @@
 * Demo app showing Pocket/RC control of a Mecanum Wheeled Robot
 */
 
-
+#define CH1_PIN 3
 
 void setup() {
-  
+  Serial.begin(115200);
+  pinMode(CH1_PIN, INPUT);
 }
 
 void loop() {
-  
+  int ch1Val = pulseIn(CH1_PIN, HIGH);
+  Serial.println(ch1Val);
 }
